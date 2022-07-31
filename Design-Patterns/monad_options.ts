f727e61f83c27f85944450fbe415318a74547c1d
Taylor Harrison Carroll <54039744+taylorhcarroll@users.noreply.github.com>
2022-07-31 17:02:27 -0500

    .

--- Design-Patterns/monad_options.ts
+++ Design-Patterns/monad_options.ts
@@ -7,17 +7,22 @@ interface Option<T> {
     Some(T)
 }
 
-
-
-function run<T>(
-    input: Option<T>,
-    transform: (_: T) => Option<T>
-): Option<T> {    
-    if (input == none) {
-        return none
+function run<T, U>(opt: Option<T>, func: (_: T) => U): Option<U> {
+    if (opt.exists()) {
+        return Option.of(func(opt.get()))
     }
-    return transform(input.value)
+    return Option.none()
 }
+
+// function run<T>(
+//     input: Option<T>,
+//     transform: (_: T) => Option<T>
+// ): Option<T> {    
+//     if (input == none) {
+//         return none
+//     }
+//     return transform(input.value)
+// }
 
 function getPetNickname(): Option<string> {
     const user: Option<User> = getCurrentUser()
